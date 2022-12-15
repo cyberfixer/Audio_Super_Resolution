f55dcb7e7fec75f0cc6612361e32d87b51e79601
WeFc <aboodWeFc@gmail.com>
2022-12-15 19:02:35 +0300

    adding more files and comments on them

---
+++ data/data_lister.py
@@ -0,0 +1,3 @@
+# this file will genarate a simple 2 txt files listing and spliting the audio files into a traning set and test set.
+# txt files will contain releitve path for every single audio file in the dataset
+# there are two files in data folder listing them 
