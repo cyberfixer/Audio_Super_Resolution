ada3c8cf0878de498266f210bedc5f135e8a31ff
Kyubravo <MOoOHMMD24@hotmail.com>
2022-12-24 16:35:52 +0300

    added audio converter script

---
+++ data/audio_high_to_low.py
@@ -0,0 +1,37 @@
+import librosa
+import soundfile as sf
+from glob import glob # get full path of all files
+from tqdm import tqdm # progress bar
+import os
+
+inputFilesPath = '.\\vctk\\48k\\'
+outputFilesPath = '.\\vctk\\8k\\'
+targetsr = 8000
+
+
+folders = glob('.\\vctk\\48k\\*')
+for folderPath in tqdm(folders, desc = 'Total'): # All folders
+    for filePath in tqdm(glob(folderPath + '\\*'),desc = os.path.basename(folderPath), leave = False): # All files in each folder
+
+        # Does the file exist?
+        if os.path.exists(targetFilePath):
+            continue
+
+        # Path Calculculations
+        fileName = os.path.basename(filePath)
+        targetFolderPath =  os.path.join(outputFilesPath + os.path.basename(folderPath))
+        targetFilePath = os.path.join(targetFolderPath, fileName)
+        targetFileExtension = os.path.splitext(fileName)[1][1:]
+
+        # Create Folders if missing
+        if os.path.isdir(outputFilesPath) is not True:
+            os.mkdir(outputFilesPath)
+        if os.path.isdir(targetFolderPath) is not True:
+            os.mkdir(targetFolderPath)
+
+        # Downsampling the audio
+        waveform, samplerate = librosa.load(filePath, sr=None)
+        waveformLow = librosa.resample(waveform,orig_sr=samplerate, target_sr=targetsr)
+        
+        # Saving the audio file.
+        sf.write(targetFilePath ,data=waveformLow, samplerate=targetsr, format=targetFileExtension)
