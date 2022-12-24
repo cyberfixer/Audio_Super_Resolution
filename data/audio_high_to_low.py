e8a8cbc556af9ccd6c8426d587f279c6f90b3d2a
Kyubravo <MOoOHMMD24@hotmail.com>
2022-12-24 18:07:56 +0300

    encapsulated everything in main()

--- data/audio_high_to_low.py
+++ data/audio_high_to_low.py
@@ -8,30 +8,33 @@ inputFilesPath = '.\\vctk\\48k'
 outputFilesPath = '.\\vctk\\8k'
 targetsr = 8000
 
+def main():
+    folders = glob(inputFilesPath + '\\*')
+    for folderPath in tqdm(folders, desc = 'Total'): # All folders
+        for filePath in tqdm(glob(folderPath + '\\*'),desc = os.path.basename(folderPath), leave = False): # All files in each folder
 
-folders = glob(inputFilesPath + '\\*')
-for folderPath in tqdm(folders, desc = 'Total'): # All folders
-    for filePath in tqdm(glob(folderPath + '\\*'),desc = os.path.basename(folderPath), leave = False): # All files in each folder
+            # Does the file exist?
+            if os.path.exists(targetFilePath):
+                continue
 
-        # Does the file exist?
-        if os.path.exists(targetFilePath):
-            continue
+            # Path Calculculations
+            fileName = os.path.basename(filePath)
+            targetFolderPath =  os.path.join(outputFilesPath + os.path.basename(folderPath))
+            targetFilePath = os.path.join(targetFolderPath, fileName)
+            targetFileExtension = os.path.splitext(fileName)[1][1:]
 
-        # Path Calculculations
-        fileName = os.path.basename(filePath)
-        targetFolderPath =  os.path.join(outputFilesPath + os.path.basename(folderPath))
-        targetFilePath = os.path.join(targetFolderPath, fileName)
-        targetFileExtension = os.path.splitext(fileName)[1][1:]
+            # Create Folders if missing
+            if os.path.isdir(outputFilesPath) is not True:
+                os.mkdir(outputFilesPath)
+            if os.path.isdir(targetFolderPath) is not True:
+                os.mkdir(targetFolderPath)
 
-        # Create Folders if missing
-        if os.path.isdir(outputFilesPath) is not True:
-            os.mkdir(outputFilesPath)
-        if os.path.isdir(targetFolderPath) is not True:
-            os.mkdir(targetFolderPath)
+            # Downsampling the audio
+            signal, samplerate = librosa.load(filePath, sr=None)
+            signalLow = librosa.resample(signal,orig_sr=samplerate, target_sr=targetsr)
+            
+            # Saving the audio file.
+            sf.write(targetFilePath ,data=signalLow, samplerate=targetsr, format=targetFileExtension)
 
-        # Downsampling the audio
-        signal, samplerate = librosa.load(filePath, sr=None)
-        signalLow = librosa.resample(signal,orig_sr=samplerate, target_sr=targetsr)
-        
-        # Saving the audio file.
-        sf.write(targetFilePath ,data=signalLow, samplerate=targetsr, format=targetFileExtension)
+if __name__ == '__main__':
+    main()
