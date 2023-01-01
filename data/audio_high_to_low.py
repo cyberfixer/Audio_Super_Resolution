2a8b9782910c7c9a3cbbf3ffbfe622b65b366363
Kyubravo <MOoOHMMD24@hotmail.com>
2023-01-01 17:04:05 +0300

    added a not-very-good multithreading

--- data/audio_high_to_low.py
+++ data/audio_high_to_low.py
@@ -3,22 +3,37 @@ import soundfile as sf
 from glob import glob  # get full path of all files
 from tqdm import tqdm  # progress bar
 import os
+import threading
 
 inputFilesPath = './data/vctk/48k'
 outputFilesPath = './data/vctk/8k'
 targetsr = 8000
 
+workers = 1
+FoldersInProcess = []
 
-def main():
-    folders = glob(inputFilesPath + '/*')
-    for folderPath in tqdm(folders, desc='Total'):  # All folders
+
+
+def innerFolders(folders):
+    lock = threading.Lock()
+    
+    for folderPath in folders:  # All folders
+        targetFolderPath = os.path.join(outputFilesPath, os.path.basename(folderPath))
+
+        # Is it already being worked on by another thread?
+        if targetFolderPath in FoldersInProcess:
+            continue
+
+        # Add the folder in foldersInProgress
+        lock.acquire()
+        FoldersInProcess.append(targetFolderPath)
+        lock.release()
+
         # All files in each folder
-        for filePath in tqdm(glob(folderPath + '/*'), desc=os.path.basename(folderPath), leave=False):
-
+        for filePath in tqdm(glob(folderPath + '/*'), desc=os.path.basename(folderPath), leave=None):
 
             # Path Calculculations
             fileName = os.path.basename(filePath)
-            targetFolderPath = os.path.join(outputFilesPath, os.path.basename(folderPath))
             targetFilePath = os.path.join(targetFolderPath, fileName)
             targetFileExtension = os.path.splitext(fileName)[1][1:]
 
@@ -39,8 +54,26 @@ def main():
 
             # Saving the audio file.
             sf.write(targetFilePath, data=signalLow,
-                     samplerate=targetsr, format=targetFileExtension)
+                    samplerate=targetsr, format=targetFileExtension)
+
+        # Remove the folder in foldersInProgress
+        lock.acquire()
+        FoldersInProcess.remove(targetFolderPath)
+        lock.release()
+
+def main():
+
+    folders = glob(inputFilesPath + '/*')
+
+    threads = [threading.Thread(target=innerFolders, args=[folders]) for i in range(workers)]
+    for i in range(workers):
+        threads[i].start()
+        
+    for i in range(workers):
+        threads[i].join()
+        
 
 
 if __name__ == '__main__':
+
     main()
