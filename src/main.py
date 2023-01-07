aa46462c8d53e6e6118972148fc2ef89b7b6e7a3
WeFc <aboodWeFc@gmail.com>
2023-01-07 22:48:20 +0300

    renaming files

--- src/main.py
+++ src/main.py
@@ -1,158 +1,190 @@
-# this file is the starting point for the model to run everything
+# known libraries
+from timeit import default_timer as timer
+import librosa
+import os
+import numpy as np
+from termcolor import colored as color
+from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-from loss import MRSTFTLossDDP
+
+# classes inside the project
+from config import CONFIG
+from model import TUNet
+from dataset import CustomDataset
+from loss import addedLoss as loss
+import metrices as m
+
+# torch libraries
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-
-from config import CONFIG
-from model import SSAR, TUNet
-from dataset import CustomDataset
-from dataset import CustomDataset
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
-import librosa
-import numpy as np
-from termcolor import colored as color
-from tqdm import tqdm
+def main():
+    #
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-# Temporarily function
+    # device agnastic code
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # creating the model and sending it to the device
+    model = TUNet().to(device)
 
+    # hyperParameters
+    BATCH_SIZE = 2
+    LR = 0.0001
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+    lr_scheduler = ReduceLROnPlateau(
+        optimizer,
+        patience=CONFIG.TRAIN.patience,
+        factor=CONFIG.TRAIN.factor,
+        verbose=True,
+    )
+    # loss class contain MSE & MRSTFTlossDDP * 10000
+    lossfn = loss()
 
-def get_power(x, nfft):
-    S = librosa.stft(x, n_fft=nfft)
-    S = np.log(np.abs(S) ** 2 + 1e-8)
-    return S
+    # importing data
+    train_data = CustomDataset("train")
+    test_data = CustomDataset("test")
+    trainDataloader = DataLoader(
+        train_data,  # dataset to turn into iterable
+        batch_size=BATCH_SIZE,  # how many samples per batch?
+        shuffle=True,  # shuffle data every epoch?
+        collate_fn=CustomDataset.collate_fn,
+    )
+    testDataloader = DataLoader(
+        test_data,  # dataset to turn into iterable
+        batch_size=BATCH_SIZE,  # how many samples per batch?
+        shuffle=True,  # shuffle data every epoch?
+        collate_fn=CustomDataset.collate_fn,
+    )
 
+    # log variables
+    # ! these lists has to be saved & loaded with the model weights
+    _trainLoss = np.empty(0)
+    _testLoss = np.empty(0)
+    _testResulte = np.empty((6, 0))
 
-def LSD(x_hr, x_pr):  # Log Spectral Distance
-    S1 = get_power(x_hr, nfft=2048)
-    S2 = get_power(x_pr, nfft=2048)
-    lsd = np.mean(np.sqrt(np.mean((S1 - S2) ** 2 + 1e-8, axis=-1)), axis=0)
-    S1 = S1[-(len(S1) - 1) // 2:, :]
-    S2 = S2[-(len(S2) - 1) // 2:, :]
-    lsd_high = np.mean(
-        np.sqrt(np.mean((S1 - S2) ** 2 + 1e-8, axis=-1)), axis=0)
-    return lsd, lsd_high
-# End of Temporarily Functions
+    # this variable will determen that is new train or will load a model
+    newTrain = True
+    if newTrain == True:
+        now = datetime.now()  # current date and time
+        # folder name for the checkpoints
+        # strftime convert time to string
+        folder = now.strftime("%m-%d %p %I-%M-%S")
+        print(folder)
+        try:
+            os.makedirs(f"checkpoints/{folder}")
+        except FileExistsError:
+            pass
+    else:
+        """this part will contain torch.load and will load all the variables needed"""
+        pass
+    epochs = 250
+    for epoch in tqdm(range(epochs), desc=f"Total", unit="Epoch"):
 
+        """Training"""
+        # Set to train mode
+        model.train()
 
-def trainStep(model: torch.nn.Module,
-              trainLoader: torch.utils.data.DataLoader,
-              lossFunction1: torch.nn.Module,
-              lossFunction2: torch.nn.Module,
-              optimizer: torch.optim.Optimizer,
-              device: torch.device = device):  # This is the starting point for training
-    # Set to train mode
-    model.train()
-
-    # Training Loop
-    for batch, (lowSignal, targetSignal) in enumerate(tqdm(trainLoader, desc="batch", unit=" batchs")):
-        # Send to GPU
-        lowSignal = lowSignal.to(device)
-        targetSignal = targetSignal.to(device)
-
-        # Forward Pass
-        predictedSignal = model(lowSignal)
-        loss = lossFunction1(predictedSignal, targetSignal) + \
-            lossFunction2(predictedSignal, targetSignal) * 10000
-
-        # Backward Pass
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    tqdm.write(color(f"Train Loss: {loss:.5f}", "blue"))
-    with open("checkpoints\log.txt", "a") as f:
-        f.write(f"-Train Loss: {loss:.5f}\n")
-    return loss
-
-
-def testStep(model: torch.nn.Module,
-             testLoader: torch.utils.data.DataLoader,
-             lossFunction1: torch.nn.Module,
-             lossFunction2: torch.nn.Module,
-             device: torch.device = device):  # This is the starting point testing the model and makeing the voice
-    # Set to test mode
-    model.eval()
-    results = []
-    resultsLSD, resultsLSDHigh = np.empty(0), np.empty(0)
-    # ! We only take the first 5 for testing purposes, remove afterwards
-    with torch.inference_mode():
-        for batch, (lowSignal, targetSignal) in enumerate(testLoader):
+        for batch, (lowSignal, targetSignal) in enumerate(tqdm(trainDataloader, desc="Epoch", unit=" batchs")):
             # Send to GPU
             lowSignal = lowSignal.to(device)
             targetSignal = targetSignal.to(device)
 
-            predictedSignal = model(lowSignal)
-            loss = lossFunction1(predictedSignal, targetSignal) + \
-                lossFunction2(predictedSignal, targetSignal) * 10000
+            # Forward Pass
+            predSignal = model(lowSignal)
 
-            lsd, LSDHigh = LSD(targetSignal.detach().cpu().numpy(),
-                               predictedSignal.detach().cpu().numpy())
+            # Calculate Loss
+            trainLoss = lossfn.loss(predSignal, targetSignal)
 
-            resultsLSD = np.append(resultsLSD, lsd)
-            resultsLSDHigh = np.append(resultsLSDHigh, LSDHigh)
+            # Zero the gradients
+            optimizer.zero_grad()
 
-    results = [resultsLSD.mean(0), resultsLSD.std(
-        0), resultsLSDHigh.mean(0), resultsLSDHigh.std(0)]
-    tqdm.write(color(f"Test Loss: {loss:.5f}", "red"))
-    tqdm.write(f"LSD Mean: {results[0]:.3f}")
-    tqdm.write(f"LSD STD: {results[1]:.3f}")
-    tqdm.write(f"LSD-HF Mean: {results[2]:.3f}")
-    tqdm.write(f"LSD-HF STD: {results[3]:.3f}")
+            # Backward Pass
+            trainLoss.backward()
 
-    with open("checkpoints\log.txt", "a") as f:
-        f.write(f"-Test Loss: {loss:.5f}\n")
-        f.write(f"LSD Mean: {results[0]:.3f}\n")
-        f.write(f"LSD STD: {results[1]:.3f}\n")
-        f.write(f"LSD-HF Mean: {results[2]:.3f}\n")
-        f.write(f"LSD-HF STD: {results[3]:.3f}\n")
+            # Update the model's parameters
+            optimizer.step()
 
+        # Train loss for the epoch
+        tqdm.write(color(f"Train Loss: {trainLoss:.5f}", "blue"))
 
-def main():
-    model = TUNet().to(device)
-    # heperparamters
-    BATCH_SIZE = 2
-    learningRate = 0.0001
-    optimizer = torch.optim.Adam(model.parameters(), lr=learningRate)
-    lossFunction1 = nn.MSELoss()
-    lossFunction2 = MRSTFTLossDDP(
-        n_bins=64, sample_rate=16000, device="cpu", scale='mel')
+        # _trainLoss will contain list of the trainLoss for every epoch
+        _trainLoss = np.append(_trainLoss, trainLoss.detach().cpu())
+        # tqdm.write(f"shape of _trainLoss: {_trainLoss.shape}")
+        # tqdm.write(f"_trainLoss: {_trainLoss})")
 
-    train_data = CustomDataset("train")
-    test_data = CustomDataset("test")
-    train_dataloader = DataLoader(train_data,  # dataset to turn into iterable
-                                  batch_size=BATCH_SIZE,  # how many samples per batch?
-                                  shuffle=False,  # shuffle data every epoch?
-                                  collate_fn=CustomDataset.collate_fn,
-                                  )
-    test_dataloader = DataLoader(test_data,  # dataset to turn into iterable
-                                 batch_size=BATCH_SIZE,  # how many samples per batch?
-                                 shuffle=False,  # shuffle data every epoch?
-                                 collate_fn=CustomDataset.collate_fn,
-                                 )
+        # lr_scheduler contain the optimizer called every epoch
+        lr_scheduler.step(trainLoss)
 
-    print(
-        f"Length of train dataloader: {len(train_dataloader)} batches of size:{BATCH_SIZE}")
-    # train_features_batch, train_labels_batch = next(iter(train_dataloader))
-    # print(train_features_batch.shape, train_labels_batch.shape)
-    epochs = 1000
-    for epoch in tqdm(range(epochs), desc=f"Total", unit=" Epochs"):
-        with open("checkpoints\log.txt", "a") as f:
-            f.write(f"----------------{epoch}----------------\n")
-        loss = trainStep(model, train_dataloader, lossFunction1,
-                         lossFunction2, optimizer)
-        # ! must use test_dataloader
-        testStep(model, test_dataloader, lossFunction1, lossFunction2)
-        PATH = f"checkpoints/Epoch{epoch}_loss{int(loss)}.pt"
+        """Testing"""
+        # Set to test mode
+        model.eval()
+        with torch.inference_mode():
+            lsdBatch = np.empty(0)
+            lsd_highBatch = np.empty(0)
+            sisdrBatch = np.empty(0)
+            for batch, (lowSignal, targetSignal) in enumerate(testDataloader):
+                # Send to GPU
+                lowSignal = lowSignal.to(device)
+                targetSignal = targetSignal.to(device)
+
+                # Forward Pass
+                predSignal = model(lowSignal)
+
+                # Calculate Loss
+                testLoss = lossfn.loss(predSignal, targetSignal)
+
+                # Calculate Metrics
+                lsd, lsd_high, sisdr = m.compute_metrics(
+                    targetSignal.detach().cpu().numpy(), predSignal.detach().cpu().numpy())
+
+                # Collacting the metrics for the whole batch
+                lsdBatch = np.append(lsdBatch, lsd)
+                lsd_highBatch = np.append(lsd_highBatch, lsd_high)
+                sisdrBatch = np.append(sisdrBatch, sisdr)
+
+            # make the list verticlly
+            batchResulte = np.vstack(
+                [lsdBatch.mean(0), lsdBatch.std(0),
+                 lsd_highBatch.mean(0), lsd_highBatch.std(0), sisdrBatch.mean(0), sisdrBatch.std(0)])
+            #
+            _testResulte = np.concatenate((_testResulte, batchResulte), axis=1)
+            # tqdm.write(f"_testResulte.shape:{_testResulte.shape}")
+            # tqdm.write(f"_testResulte:{_testResulte}")
+
+            # test Loss for every epoch
+            tqdm.write(color(f"Test Loss: {testLoss:.5f}", "red"))
+            tqdm.write(f"LSD: {batchResulte[0]}")
+            tqdm.write(f"LSD-High: {batchResulte[2]}")
+            tqdm.write(f"SI-SDR: {batchResulte[4]}")
+            _testLoss = np.append(_testLoss, testLoss.detach().cpu())
+
+        # TODO: save the model and its variables
+        PATH = f"./checkpoints/{folder}/Epoch{epoch}_loss{int(_testLoss[-1])}.pt"
         torch.save({
             'epoch': epoch,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-            'loss': loss,
+            '_trainloss': _trainLoss,
+            '_testloss': _testLoss,
+            '_testResulte': _testResulte
         }, PATH)
+    # epochlist = list(range(1, epoch+2))
+    # fig, ax = plt.subplots(nrows=1, ncols=3)
+    # ax[0].plot(epochlist, _testResulte[0])
+    # ax[0].set_xlabel("Epochs")
+    # ax[0].set_title("LSD")
+    # ax[1].plot(epochlist, _testResulte[2])
+    # ax[1].set_xlabel("Epochs")
+    # ax[1].set_title("LSD_High")
+    # ax[2].plot(epochlist, _testResulte[4])
+    # ax[2].set_xlabel("Epochs")
+    # ax[2].set_title("SI_SDR")
+    # plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
