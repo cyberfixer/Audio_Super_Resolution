fb130063017c9c7b4029c746cd2f2994cd161905
WeFc <aboodWeFc@gmail.com>
2023-01-12 17:58:44 +0300

    high_low maltithread

--- data/audio_high_to_low.py
+++ data/audio_high_to_low.py
@@ -3,17 +3,28 @@ import soundfile as sf
 from glob import glob  # get full path of all files
 from tqdm import tqdm  # progress bar
 import os
+import threading
+import time
 
 inputFilesPath = './data/vctk/48k'
-outputFilesPath = './data/vctk/8k16k'
+outputFilesPath = './data/vctk/test'
 
 targetsr = 16000
 datasr = 8000
 
+folderinuse = []
 
-def main():
+
+def main(lock):
     folders = glob(inputFilesPath + '/*')
     for folderPath in tqdm(folders, desc='Total',  dynamic_ncols=True):  # All folders
+        lock.acquire()
+        if folderPath in folderinuse:
+            lock.release()
+            continue
+        else:
+            folderinuse.append(folderPath)
+            lock.release()
         # All files in each folder
         for filePath in tqdm(glob(folderPath + '/*'), desc=os.path.basename(folderPath), leave=False, dynamic_ncols=True):
 
@@ -49,7 +60,28 @@ def main():
             # Saving the audio file.
             sf.write(targetFilePath, data=signalLow,
                      samplerate=targetsr, format=targetFileExtension)
+        lock.acquire()
+        folderinuse.remove(folderPath)
+        lock.release()
+
+
+def make_threads(workers):
+    print(f"Number of threads: {workers}")
+    # intialize
+    threads_list = []
+    lock = threading.Lock()
+    for i in range(workers):
+        threads_list.append(threading.Thread(target=main, args=(lock,)))
+
+    # start threads
+    for i in threads_list:
+        time.sleep(1)
+        i.start()
+
+    # wait until threads finish their job
+    for i in threads_list:
+        i.join()
 
 
 if __name__ == '__main__':
-    main()
+    make_threads(6)
